6d65ac2275649fe413ab7d5af7d1f56a16bf677c
Idris Raja <idris.raja@gmail.com>
2023-04-30 07:14:32 -0500

    add starship

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -2,20 +2,47 @@
   config = {
     programs.starship = {
       enable = true;
-      # settings = {
-      # add_newline = false;
-      # format = lib.concatStrings [
-      # "$line_break"
-      # "$package"
-      # "$line_break"
-      # "$character"
-      # ];
-      # scan_timeout = 10;
-      # character = {
-      # success_symbol = "➜";
-      # error_symbol = "➜";
-      # };
-      # };
+      settings = {
+        add_newline = false;
+        scan_timeout = 10;
+        character = {
+          success_symbol = "➜";
+          error_symbol = "➜";
+        };
+
+        directory = {
+          truncation_length = 4;
+          truncation_symbol = "…/";
+        };
+
+        aws = { disabled = true; };
+
+        git_metrics = { disabled = false; };
+
+        git_status = {
+          conflicted = "🏳";
+          ahead = "🏎💨";
+          behind = "😰";
+          diverged = "😵";
+          up_to_date = "✓";
+          untracked = "🤷";
+          stashed = "📦";
+          modified = "📝";
+          staged = "[++($count)](green)";
+          renamed = "👅";
+          deleted = "🗑";
+        };
+
+        nix_shell = {
+          disabled = false;
+          format = " [$symbol$state]($style) ";
+        };
+
+        haskell = {
+          disabled = false;
+          format = " [$symbol($version )]($style)";
+        };
+      };
     };
   };
 }
