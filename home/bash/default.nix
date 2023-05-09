7e4be4bc8677051a874990ff07dc4679f5916d0f
Idris Raja <idris.raja@gmail.com>
2023-05-09 07:35:41 -0500

    add bash

---
+++ home/bash/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, config, ... }: {
+  config = {
+    programs.bash = {
+      initExtraFirst = ''
+        set -o vi
+      '';
+
+      enable = true;
+      history.extended = true;
+      enableSyntaxHighlighting = true;
+      enableCompletion = true;
+    };
+  };
+}
