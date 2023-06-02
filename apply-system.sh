cccfe726a7e03c6788455575b72e7735ddc0af15
Idris Raja <idris.raja@gmail.com>
2023-06-02 18:58:23 -0500

    move location of flake files

--- apply-system.sh
+++ apply-system.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-# shellcheck disable=SC3010 # [[ undefined
-# shellcheck disable=SC3044 # popd and pushd undefined in posix
 
-set -e
-set -u
-
-pushd ~/dotfiles/system
-# sudo nixos-rebuild switch -I nixos-config=configuration.nix
 sudo nixos-rebuild switch --flake .#surface2
-popd
--- flake.nix
+++ flake.nix
@@ -3,14 +3,12 @@
 
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-
     nixos-hardware = {
       type = "github";
       owner = "nixos";
       repo = "nixos-hardware";
       rev = "3023004e9903bc2f726da7c4a6724cf55f45bfff";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,13 +22,13 @@
         surface2 = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
-            ./configuration.nix
+            ./system/configuration.nix
             nixos-hardware.nixosModules.microsoft-surface-pro-intel
             home-manager.nixosModules.home-manager
             {
               # home-manager.useGlobalPkgs = true;
               # home-manager.useUserPackages = true;
-              home-manager.users.hippoid = import ../home/base.nix;
+              home-manager.users.hippoid = import home/base.nix;
             }
           ];
         };
