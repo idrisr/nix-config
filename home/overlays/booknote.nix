c8c46fd30085872adfb94c142547f8e9be83f5ba
Idris Raja <idris.raja@gmail.com>
2023-06-03 18:49:46 -0500

    remove overlays

--- home/overlays/booknote.nix
+++
@@ -1,8 +0,0 @@
-final: prev: {
-  booknote = final.writeShellApplication {
-    name = "booknote";
-
-    runtimeInputs = with final; [ pdftc ];
-    text = builtins.readFile ./booknote.sh;
-  };
-}
