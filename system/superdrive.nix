1b78f578a29ec0e38e3098984d53adc343cb1c67
Idris Raja <idris.raja@gmail.com>
2023-06-22 21:54:06 -0500

    fix udev rule

--- system/superdrive.nix
+++ system/superdrive.nix
@@ -1,9 +1,10 @@
 { config, pkgs, ... }: {
+  # https://kuziel.nz/notes/2018/02/apple-superdrive-linux.html
   config = {
     services.udev.extraRules = ''
       ACTION=="add", ATTRS{idProduct}=="1500", ATTRS{idVendor}=="05ac",
-      DRIVERS=="usb", RUN+="${pkgs.sg_utils}/bin/sg_raw /dev/$kernel EA 00 00 00 00 00 01"'';
+      DRIVERS=="usb", RUN+="${pkgs.sg3_utils}/bin/sg_raw /dev/$kernel EA 00 00 00 00 00 01"'';
   };
-  options = [ ];
-  imports = { };
+  options = { };
+  imports = [ ];
 }
