cbfefc798bbc7649675e6f3a284a4bcec5ba5bb0
Idris Raja <idris.raja@gmail.com>
2023-07-04 11:24:45 -0500

    trying to get overlay working

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "my machines' nixos configuration";
 
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    nixpkgs.url = "nixpkgs";
     nixos-hardware = {
       type = "github";
       owner = "nixos";
@@ -17,28 +17,50 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    knotools = {
+      url = "github:idrisr/knotools";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, nixos-hardware, home-manager, nixos-generators, ... }:
+  outputs =
+    { nixpkgs, nixos-hardware, home-manager, nixos-generators, knotools, ... }:
     let
       system = "x86_64-linux";
       common = [
         ./system/configuration.nix
         home-manager.nixosModules.home-manager
-        { home-manager.users.hippoid = import home/base.nix; }
-
+        { config = { nixpkgs.overlays = [ knotools.overlays ]; }; }
+        {
+          home-manager = {
+            users.hippoid = import home/base.nix;
+            useUserPackages = true;
+          };
+        }
       ];
-      pkgs = import nixpkgs { system = system; };
+      pkgs = import nixpkgs { inherit system; };
+
     in {
       packages.x86_64-linux = {
         install = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
+          inherit system;
           modules = [ ./system/hardware-framework.nix ] ++ common;
           format = "install-iso";
         };
       };
-
       nixosConfigurations = {
+        fft = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ ./system/hardware-fft.nix ] ++ common;
+        };
+        framework = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ ./system/hardware-framework.nix ] ++ common;
+        };
+        red = nixpkgs.lib.nixosSystem {
+          system = "i686-linux";
+          modules = [ ./system/hardware-red.nix ] ++ common;
+        };
         surface = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
@@ -46,32 +68,15 @@
             nixos-hardware.nixosModules.microsoft-surface-pro-intel
           ] ++ common;
         };
-
-        fft = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./system/hardware-fft.nix ] ++ common;
-        };
-
-        framework = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./system/hardware-framework.nix ] ++ common;
-        };
-
-        red = nixpkgs.lib.nixosSystem {
-          system = "i686-linux";
-          modules = [ ./system/hardware-red.nix ] ++ common;
-        };
       };
-
       devShells.x86_64-linux.default = with pkgs;
         mkShell {
           buildInputs = [
-            ghcid
-            (ghc.withPackages (p: with p; [ xmonad xmonad-extras dbus ]))
-            haskell-language-server
-            luajitPackages.lua-lsp
-            nodePackages.vim-language-server
-
+            # ghcid
+            # (ghc.withPackages (p: with p; [ xmonad xmonad-extras dbus ]))
+            # haskell-language-server
+            # luajitPackages.lua-lsp
+            # nodePackages.vim-language-server
           ];
         };
     };
