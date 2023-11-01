3acb6481dcb02444c7ead680e01d0e3be4df000c
Idris Raja <idris.raja@gmail.com>
2023-11-01 17:58:34 -0500

    more borg

--- system/borgrepo.nix
+++ system/borgrepo.nix
@@ -1,8 +1,16 @@
 { config, pkgs, ... }: {
   config = {
-    users.users.hippoid = {
-      openssh.authorizedKeys.keys =
-        [ (builtins.readFile ./public-keys/id_rsa.pub) ];
+    users.users.borg = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0cwEynaFR5ZsIcKlV/3Ca1vUFg3YJ/3+Gv7l0YVVjZ root@surface"
+      ];
+    };
+    services.borgbackup.repos = {
+      my_borg_repo = {
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0cwEynaFR5ZsIcKlV/3Ca1vUFg3YJ/3+Gv7l0YVVjZ root@surface"
+        ];
+      };
     };
   };
 }
