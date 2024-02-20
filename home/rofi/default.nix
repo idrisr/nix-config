2586aae18dc774161197d0481d43b6ec79b438db
Idris Raja <idris.raja@gmail.com>
2024-02-19 20:56:19 -0600

    make rofi better

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,13 +1,16 @@
-{ pkgs, ... }:
-
-{
+{ pkgs, ... }: {
   config = {
     programs.rofi = {
       enable = true;
       terminal = "${pkgs.alacritty}/bin/alacritty";
       font = "hack 24";
       location = "center";
-      theme = ./nord.rasi;
+      plugins = with pkgs; [ rofi-calc rofi-emoji rofi-power-menu ];
+      extraConfig = {
+        modi = "drun,emoji,ssh";
+        kb-row-up = "Up,Alt+k";
+        kb-row-down = "Down,Alt+j";
+      };
     };
   };
 }
