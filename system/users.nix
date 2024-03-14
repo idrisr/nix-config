f161ae170a036c61045c31406d8ae9987945300a
Idris Raja <idris.raja@gmail.com>
2024-03-14 07:13:05 -0500

    key change

--- system/users.nix
+++ system/users.nix
@@ -17,7 +17,7 @@
       hashedPassword =
         "$y$j9T$AtpzctqNX9eOED3dkj1es/$gAwm9CkJRZolwuv9kC4CGlXd7znho1EqmcJJZHum6d5";
       openssh.authorizedKeys.keys =
-        [ (builtins.readFile ./public-keys/id_rsa.pub) ];
+        [ (builtins.readFile ./public-keys/id_ed25519.pub) ];
     };
   };
 }
