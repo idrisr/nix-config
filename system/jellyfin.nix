7f15e538863c9e48f768d71ba436a161561976c5
Idris Raja <idris.raja@gmail.com>
2024-04-03 13:07:30 -0500

    jellyfin

--- system/jellyfin.nix
+++ system/jellyfin.nix
@@ -1,6 +1,14 @@
-{
-  config.services.jellyfin = {
-    enable = true;
-    openFirewall = true;
+{ pkgs, ... }: {
+  config = {
+    services.jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
+    users.users.jellyfin = {
+      description = "jellyfin";
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys =
+        [ (builtins.readFile ./public-keys/jellyfin_ed25519.pub) ];
+    };
   };
 }
