aff731c2100cd2e7141f09871a9c7b5643267e57
Idris Raja <idris.raja@gmail.com>
2024-04-22 19:01:31 -0500

    no more ddg

--- home-manager-modules/modules/qutebrowser/default.nix
+++ home-manager-modules/modules/qutebrowser/default.nix
@@ -27,22 +27,20 @@
         };
 
         url = {
-          start_pages = "https://duckduckgo.com";
-          default_page = "https://duckduckgo.com";
+          start_pages = "https://search.brave.com/";
+          default_page = "https://search.brave.com/";
         };
       };
 
       searchEngines = {
         # DEFAULT = "https://kagi.com/search?q={}";
-        DEFAULT = "https://duckduckgo.com/?q={}";
+        DEFAULT = "https://search.brave.com/search?q={}";
         d3 = "https://duckduckgo.com/?q=site%3Ad3js.org+{}";
         dd = "https://duckduckgo.com/?q={}";
         aa = "https://annas-archive.org/search?q={}";
         am = "https://www.amazon.com/s?k={}";
         amz = "https://www.amazon.com/s?k={}&i=stripbooks";
-        c = "https://cplusplus.com/search.do?q={}";
         cpl = "https://chipublib.bibliocommons.com/v2/search?query={}";
-        git = "https://github.com/search?q={}";
         ho = "https://hoogle.haskell.org/?hoogle={}";
         hoe = "https://hoogle.haskell.org/?hoogle={}%20is%3Aexact";
         hm =
@@ -50,13 +48,8 @@
         mdn = "https://developer.mozilla.org/en-US/search?q={}";
         np = "https://search.nixos.org/packages?query={}&channel=unstable";
         no = "https://search.nixos.org/options?query={}&channel=unstable";
-        nixpkgs-issues =
-          "https://github.com/NixOS/nixpkgs/issues?q=is%3Aopen+{}";
-        nixpkgs-prs = "https://github.com/NixOS/nixpkgs/pulls?q=is%3Aopen+{}";
         npl =
           "https://encore.naperville-lib.org/iii/encore/search/C__S{}__Orightresult__U?lang=eng";
-        py = "https://docs.python.org/3/search.html?q={}";
-        repology = "https://repology.org/projects/?search={}";
         yt = "https://www.youtube.com/results?search_query={}";
       };
 
@@ -67,7 +60,6 @@
         nixos-manual = "https://nixos.org/nixos/manual";
         nixpkgs = "https://github.com/NixOS/nixpkgs";
         nixpkgs-manual = "https://nixos.org/manual/nixpkgs/unstable";
-        youtube = "https://www.youtube.com/";
         zh = "https://zerohedge.com/";
       };
     };
