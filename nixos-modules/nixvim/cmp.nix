dd2d79113fbefeb08a80b33d69d45c05716ecd1d
Idris Raja <idris.raja@gmail.com>
2024-04-26 10:10:47 -0500

    vim changes

--- nixos-modules/nixvim/cmp.nix
+++ nixos-modules/nixvim/cmp.nix
@@ -1,11 +1,28 @@
 {
-  config.programs.nixvim.plugins = {
-    cmp.enable = true;
-    cmp-buffer.enable = true;
-    cmp-path.enable = true;
-    cmp-cmdline.enable = true;
-    cmp-nvim-ultisnips.enable = true;
-    cmp-nvim-lsp.enable = true;
-    friendly-snippets.enable = true;
+  config.programs.nixvim = {
+    keymaps = [
+      {
+        key = "<leader>cd";
+        action = ":lua require('cmp').setup.buffer { enabled = false }<cr>";
+        mode = "n";
+        options = { desc = "disable cmp"; };
+      }
+      {
+        key = "<leader>ce";
+        action = ":lua require('cmp').setup.buffer { enabled = true}<cr>";
+        mode = "n";
+        options = { desc = "enable cmp"; };
+      }
+    ];
+
+    plugins = {
+      cmp.enable = true;
+      cmp-buffer.enable = true;
+      cmp-path.enable = true;
+      cmp-cmdline.enable = true;
+      cmp-nvim-ultisnips.enable = true;
+      cmp-nvim-lsp.enable = true;
+      friendly-snippets.enable = true;
+    };
   };
 }
