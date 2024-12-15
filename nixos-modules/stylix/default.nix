adff072b1a183717038a2ee79071808df16d9202
Idris Raja <idris.raja@gmail.com>
2024-12-15 08:19:36 -0600

    winter wallpaper

--- nixos-modules/stylix/default.nix
+++ nixos-modules/stylix/default.nix
@@ -2,10 +2,11 @@
   stylix = {
     enable = true;
     image = pkgs.fetchurl {
+      # url = "https://www.pixelstalk.net/wp-content/uploads/image11/Winter-HD-Desktop-Wallpaper-with-pink-winter-dawn-over-a-snowy-mountain-range.jpg";
+      # sha256 = "R26yTKPqV/snIifyXdB/sY0UMjDC9ziY6xMJQoOaD4M=";
       url =
-        # "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-        "https://www.pixelstalk.net/wp-content/uploads/image11/Winter-HD-Desktop-Wallpaper-with-pink-winter-dawn-over-a-snowy-mountain-range.jpg";
-      sha256 = "R26yTKPqV/snIifyXdB/sY0UMjDC9ziY6xMJQoOaD4M=";
+        "https://www.pixelstalk.net/wp-content/uploads/2015/01/Winter-Landscape-Snow-Background.jpg";
+      sha256 = "BIogp6FNN9lHNcNtgP9tdIUWLtP1EAAHJIpnNGPO7W0=";
     };
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
