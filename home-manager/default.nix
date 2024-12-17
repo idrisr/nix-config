085910b37ffc03f26946e258d5447715941635a4
Idris Raja <idris.raja@gmail.com>
2024-12-17 10:37:56 -0600

    make hm stop complaining

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -5,6 +5,7 @@ in {
   config.home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+    backupFileExtension = "backup";
     extraSpecialArgs = {
       inherit inputs;
       inherit host;
