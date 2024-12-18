abd680bc19f61d1221e0d7bc1b8c4379b747c15a
Idris Raja <idris.raja@gmail.com>
2024-12-17 20:11:18 -0600

    tune atuin

--- home-manager/modules/atuin/default.nix
+++ home-manager/modules/atuin/default.nix
@@ -4,6 +4,7 @@
       enable = true;
 
       daemon.enable = false;
+      flags = [ "--disable-up-arrow" ];
       settings = {
         filter_mode_shell_up_key_binding = "session";
         inline_height = 30;
