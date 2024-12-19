ae6b1711747db2a677b8617aada7debe84bd1aa8
Idris Raja <idris.raja@gmail.com>
2024-12-18 21:10:32 -0600

    fix device

--- nixos-modules/syncthing/default.nix
+++ nixos-modules/syncthing/default.nix
@@ -15,7 +15,7 @@
       configDir = "/home/hippoid/.config/syncthing";
       devices = {
         surface.id =
-          "4ACO4YT-J3BBDR7-SMZPHBN-IK7KGSI-5TOIQIC-YXYOC5U-3SQDFY7-IJ6TOQA";
+          "5BM7GQY-F3QYSDR-UEW2GMA-IICKXDD-NFH5BEE-TSU4TIN-BSBGSPQ-JEPLSAJ";
         framework.id =
           "PNAM4JI-5E5CRM6-YVD6CT4-Z6D7XQD-JARNY3R-FLGG2YY-62IHSVV-ARVFGAY";
       };
