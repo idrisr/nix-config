b1056c5a625c05340a6b8ec641dd23ed06ee968e
Idris Raja <idris.raja@gmail.com>
2024-12-19 07:52:30 -0600

    syncing

--- nixos-modules/syncthing/default.nix
+++ nixos-modules/syncthing/default.nix
@@ -1,4 +1,17 @@
-{
+let
+  mkFolderShare = name: {
+    path = "/home/hippoid/${name}";
+    devices = [ "surface" "framework" ];
+    vesioning = {
+      type = "staggered";
+      params = {
+        cleanInterval = "3600";
+        maxAge = "15768000";
+      };
+    };
+  };
+
+in {
   services = {
     syncthing = {
       enable = true;
@@ -7,30 +20,27 @@
       overrideFolders = true;
       user = "hippoid";
       group = "users";
-      guiAddress = "0.0.0.0:8384";
-
-      settings.options.urAccepted = -1;
-      settings.options.relaysEnabled = false;
+      guiAddress = "localhost:8384";
 
       configDir = "/home/hippoid/.config/syncthing";
-      devices = {
-        surface.id =
-          "5BM7GQY-F3QYSDR-UEW2GMA-IICKXDD-NFH5BEE-TSU4TIN-BSBGSPQ-JEPLSAJ";
-        framework.id =
-          "PNAM4JI-5E5CRM6-YVD6CT4-Z6D7XQD-JARNY3R-FLGG2YY-62IHSVV-ARVFGAY";
-      };
+      settings = {
+        options.urAccepted = -1;
+        options.relaysEnabled = false;
+        devices = {
+          surface.id =
+            "5BM7GQY-F3QYSDR-UEW2GMA-IICKXDD-NFH5BEE-TSU4TIN-BSBGSPQ-JEPLSAJ";
+          framework.id =
+            "PNAM4JI-5E5CRM6-YVD6CT4-Z6D7XQD-JARNY3R-FLGG2YY-62IHSVV-ARVFGAY";
+        };
 
-      folders = {
-        "syncme" = {
-          path = "/home/hippoid/roam-export";
-          devices = [ "surface" "framework" ];
-          vesioning = {
-            type = "staggered";
-            params = {
-              cleanInterval = "3600";
-              maxAge = "15768000";
-            };
-          };
+        folders = {
+          "fun" = mkFolderShare "fun";
+          "books" = mkFolderShare "books";
+          "downloads" = mkFolderShare "downloads";
+          "videos" = mkFolderShare "videos";
+          "documents" = mkFolderShare "documents";
+          "roam-export" = mkFolderShare "roam-export";
+          "screenshots" = mkFolderShare "screenshots";
         };
       };
     };
