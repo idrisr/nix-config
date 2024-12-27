89d2bbfb1a196ead655229c8b6314ad9d543c211
Idris Raja <idris.raja@gmail.com>
2024-12-27 12:42:02 -0600

    framework specific

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -5,7 +5,11 @@
     base.enable = true;
     borg-backup-client.enable = true;
     display.enable = true;
-    environment.systemPackages = with pkgs; [ framework-tool intel-gpu-tools ];
+    environment.systemPackages = with pkgs; [
+      framework-tool
+      intel-gpu-tools
+      brightnessctl
+    ];
     profile = {
       dailydrive.enable = true;
       rofi-book-search.enable = true;
