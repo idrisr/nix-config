181b511631c21fd4a2a8deb2ef684f5a316cc3b2
Idris Raja <idris.raja@gmail.com>
2025-01-21 19:04:44 -0600

    290 current  2025-01-21 19:04:27  25.05.20250116.5df4362   6.6.71                          *

--- nixos-modules/nixvim/init.lua
+++ nixos-modules/nixvim/init.lua
@@ -25,29 +25,4 @@ require("lspconfig").lua_ls.setup({
 	},
 })
 
-require("lspconfig").texlab.setup({
-	symbols = {
-		customEnvironments = { "definition" },
-	},
-	auxDirectory = ".",
-	bibtexFormatter = "texlab",
-	build = {
-		args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-		executable = "latexmk",
-		forwardSearchAfter = false,
-		onSave = false,
-	},
-	chktex = {
-		onEdit = false,
-		onOpenAndSave = true,
-	},
-	diagnosticsDelay = 300,
-	formatterLineLength = 80,
-	forwardSearch = {
-		args = {},
-	},
-	latexFormatter = "latexindent",
-	latexindent = {
-		modifyLineBreaks = false,
-	},
-})
+require("lspconfig").texlab.setup({})
