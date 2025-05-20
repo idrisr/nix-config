acb26099dc3b9924de8de8cd6c4f21f2ee196202
Idris Raja <idris.raja@gmail.com>
2025-05-20 08:04:27 -0500

    printer

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -4,13 +4,18 @@
     monitoring.enable = false;
     base.enable = true;
     borg-backup-client.enable = true;
+    local.printer.enable = true;
     display.enable = true;
+    nvr.enable = false;
     virtualization.enable = true;
     environment.systemPackages = with pkgs; [
       framework-tool
       intel-gpu-tools
       brightnessctl
       fprintd
+      tree
+      openssl
+      vifm
     ];
     profile = {
       dailydrive.enable = true;
--- nixos-modules/printer/printer-client.nix
+++ nixos-modules/printer/printer-client.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
-let
-  printer_name = "Brother_HL-L2300D_series";
-  cfg = config.local.printer;
+let cfg = config.local.printer;
 in {
   options = {
     local.printer = {
@@ -16,21 +14,21 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
+      drivers = [ pkgs.brlaser ];
+    };
 
     hardware.printers = {
-      # # ensurePrinters = [{
-      # # name = printer_name;
-      # # location = "3112";
-      # # deviceUri = "ipp://BRW6814015277E8.local:631/ipp/print";
-      # # # deviceUri =
-      # # # "dnssd://Brother%20HL-L2300D%20series%20%40%20red._ipp._tcp.local/cups?uuid=5cfc6423-a6d0-34cc-5cdb-47d50800ef6f";
-      # # # model = "drv:///brlaser.drv/brl2300d.ppd";
-      # # model = "";
-      # # ppdOptions = { PageSize = "A4"; };
-      # # }];
-      ensureDefaultPrinter = printer_name;
+      ensurePrinters = [{
+        name = "bro2300";
+        deviceUri = "usb://Brother/HL-L2300D%20series?serial=U63878J1N698009";
+        model = "drv:///brlaser.drv/brl2300d.ppd";
+      }];
+      ensureDefaultPrinter = "bro2300";
     };
+
+    users.users.hippoid.extraGroups = [ "lpadmin" ];
     environment.systemPackages = [ pkgs.brlaser ];
   };
 }
