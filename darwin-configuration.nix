583b0c8ff6ca5fcf4820aee8f9e665f2c80499f2
Idris Raja <idris.raja@gmail.com>
2026-07-23 17:18:10 -0500

    format darwin configuration

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,4 +1,9 @@
-{ inputs, host, user, system }:
+{
+  inputs,
+  host,
+  user,
+  system,
+}:
 {
   darwinConfigurations.${host} = inputs.nix-darwin.lib.darwinSystem {
     inherit system;
@@ -6,42 +11,51 @@
       inherit inputs host user;
     };
     modules = [
-      ({ pkgs, ... }: {
-        nix.settings.experimental-features = [ "nix-command" "flakes" ];
-        nixpkgs.hostPlatform = system;
-        nixpkgs.config.allowUnfree = true;
-        networking.hostName = host;
-        system.primaryUser = user;
-        users.users.${user}.home = "/Users/${user}";
+      (
+        { pkgs, ... }:
+        {
+          nix.settings.experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          nixpkgs.hostPlatform = system;
+          nixpkgs.config.allowUnfree = true;
+          networking.hostName = host;
+          system.primaryUser = user;
+          users.users.${user}.home = "/Users/${user}";
 
-        environment.systemPackages = with pkgs; [
-          git
-          curl
-          prometheus-node-exporter
-        ];
+          environment.systemPackages = with pkgs; [
+            git
+            curl
+            prometheus-node-exporter
+          ];
 
-        system.stateVersion = 4;
-      })
+          system.stateVersion = 4;
+        }
+      )
       ./hosts/macbook/default.nix
 
       inputs.home-manager.darwinModules.home-manager
-      ({ pkgs, ... }: {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = {
-          inputs = inputs // {
-            "idris-pkgs" = inputs.home-config;
+      (
+        { pkgs, ... }:
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {
+            inputs = inputs // {
+              "idris-pkgs" = inputs.home-config;
+            };
+            inherit pkgs;
+            graphical = true;
           };
-          inherit pkgs;
-          graphical = true;
-        };
-        home-manager.users.${user} = {
-          imports = [
-            (inputs."home-config" + "/home.nix")
-            ./hosts/macbook/home.nix
-          ];
-        };
-      })
+          home-manager.users.${user} = {
+            imports = [
+              (inputs."home-config" + "/home.nix")
+              ./hosts/macbook/home.nix
+            ];
+          };
+        }
+      )
     ];
   };
 }
